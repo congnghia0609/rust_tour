d9ff958775b3349ac01678dd9d4bbdd2f646c1ec
congnghia0609 <congnghia0609@gmail.com>
2020-12-25 00:40:03 +0700

    add Expressions

--- README.md
+++ README.md
@@ -622,3 +622,26 @@ fn main() {
     //=> Sum: 15
 }
 ```
+
+### 3. Expressions
+```rust
+fn main() {
+    let x = 2u32;
+    let y = {
+        let x_squared = x * x;
+        let x_cube = x_squared * x;
+        // This expression will be assigned to `y` => no semicolon
+        x_cube + x_squared + x
+    };
+    let z = {
+        // The semicolon suppresses this expression and `()` is assigned to `z`
+        2 * x;
+    };
+    println!("x is {:?}", x);
+    println!("y is {:?}", y);
+    println!("z is {:?}", z);
+    // x is 2
+    // y is 14
+    // z is ()
+}
+```
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,22 @@
-use std::fmt;
 
-struct Circle {
-    radius: i32,
-}
-
-impl fmt::Display for Circle {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        println!("In fn Display Circle");
-        write!(f, "Circle of radius {}", self.radius)
-    }
-}
 
 fn main() {
-    let circle =Circle {radius: 6};
-    println!("{}", circle.to_string());
-    //=> Circle of radius 6
-
-    let parsed: i32 = "5".parse().unwrap();
-    let turbo_parsed = "10".parse::<i32>().unwrap();
-    let sum = parsed + turbo_parsed;
-    println!("Sum: {:?}", sum);
-    //=> Sum: 15
+    let x = 2u32;
+    let y = {
+        let x_squared = x * x;
+        let x_cube = x_squared * x;
+        // This expression will be assigned to `y` => no semicolon
+        x_cube + x_squared + x
+    };
+    let z = {
+        // The semicolon suppresses this expression and `()` is assigned to `z`
+        2 * x;
+    };
+    println!("x is {:?}", x);
+    println!("y is {:?}", y);
+    println!("z is {:?}", z);
+    // x is 2
+    // y is 14
+    // z is ()
 }
 
