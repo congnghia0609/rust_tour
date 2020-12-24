af195ab38307feea466b13d8d5b82139f369c791
congnghia0609 <congnghia0609@gmail.com>
2020-12-25 00:04:57 +0700

    add TryFrom TryInto

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,29 @@
-use std::convert::From;
+use std::convert::TryFrom;
+use std::convert::TryInto;
 
-#[derive(Debug)]
-struct Number {
-    value: i32,
-}
+#[derive(Debug, PartialEq)]
+struct EvenNumber (i32);
 
-impl From<i32> for Number {
-    fn from(item: i32) -> Self {
-        println!("In fn From Number");
-        Number {value: item}
+impl TryFrom<i32> for EvenNumber {
+    type Error = ();
+    fn try_from(value: i32) -> Result<Self, Self::Error> {
+        println!("In fn TryFrom EvenNumber");
+        if value % 2 == 0 {
+            Ok(EvenNumber(value))
+        } else {
+            Err(())
+        }
     }
 }
 
 fn main() {
-    let num = Number::from(30);
-    println!("My number is {:?}", num);
-    //=> My number is Number { value: 30 }
-
-    let int  = 5;
-    let num2: Number = int.into(); // call Number::from(int);
-    println!("My number is {:?}", num2);
-    //=> My number is Number { value: 5 }
+    // TryFrom
+    assert_eq!(EvenNumber::try_from(8), Ok(EvenNumber(8)));
+    assert_eq!(EvenNumber::try_from(5), Err(()));
+    // TryInto
+    let result: Result<EvenNumber, ()> = 8i32.try_into();
+    assert_eq!(result, Ok(EvenNumber(8)));
+    let result: Result<EvenNumber, ()> = 5i32.try_into();
+    assert_eq!(result, Err(()));
 }
 
