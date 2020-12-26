cce57028582d0e573c8d35d77b443dba130576a3
congnghia0609 <congnghia0609@gmail.com>
2020-12-27 02:37:58 +0700

    add Diverging functions

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,20 @@
-fn is_odd(n: u32) -> bool {
-    n % 2 == 1
-}
-
 fn main() {
-    println!("Find the sum of all the squared odd numbers under 1000");
-    let upper = 1000;
-
-    // Imperative approach
-    // Declare accumulator variable
-    let mut acc = 0;
-    // Iterate: 0, 1, 2, ... to infinity
-    for n in 0.. {
-        // Square the number
-        let n_squared = n * n;
-        if n_squared >= upper {
-            // Break loop if exceeded the upper limit
-            break;
-        } else if is_odd(n_squared) {
-            // Accumulate value, if it's odd
-            acc += n_squared;
+    fn sum_odd_numbers(up_to: u32) -> u32 {
+        let mut acc = 0;
+        for i in 0..up_to {
+            // Notice that the return type of this match expression must be u32
+            // because of the type of the "addition" variable.
+            let addition: u32 = match i%2 == 1 {
+                // The "i" variable is of type u32, which is perfectly fine.
+                true => i,
+                // On the other hand, the "continue" expression does not return
+                // u32, but it is still fine, because it never returns and therefore
+                // does not violate the type requirements of the match expression.
+                false => continue,
+            };
+            acc += addition;
         }
+        acc
     }
-    println!("imperative style: {}", acc);
-    //=> imperative style: 5456
-
-    // Functional approach
-    let sum_of_squared_odd_numbers: u32 =
-        (0..).map(|n| n * n)                                // All natural numbers squared
-            .take_while(|&n_squared| n_squared < upper)     // Below upper limit
-            .filter(|&n_squared| is_odd(n_squared))      // That are odd
-            .fold(0, |acc, n_squared| acc + n_squared);           // Sum them
-    println!("functional style: {}", sum_of_squared_odd_numbers);
-    //=> functional style: 5456
+    println!("Sum of odd numbers up to 9 (excluding): {}", sum_odd_numbers(9));
 }
