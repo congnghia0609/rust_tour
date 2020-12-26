3d926265130e9cc08ca847693b2270e03d943fc7
congnghia0609 <congnghia0609@gmail.com>
2020-12-26 23:44:08 +0700

    add while let

--- README.md
+++ README.md
@@ -1114,6 +1114,27 @@ fn main() {
 
 #### 4.7. While let
 ```rust
-
+fn main() {
+    // Make optional of type Option<i32>
+    let mut optional = Some(0);
+    // This reads: while let destrutures optional into
+    // Some(i), evaluate the block({}). Else break.
+    while let Some(i) = optional {
+        if i > 3 {
+            println!("Greater than 3, quit!");
+            optional = None;
+        } else {
+            println!("i is {:?}. Try again.", i);
+            optional = Some(i + 1);
+        }
+        // ^ Less rightward drift and doesn't require
+        // explicitly handling the failing case.
+    }
+    // i is 0. Try again.
+    // i is 1. Try again.
+    // i is 2. Try again.
+    // i is 3. Try again.
+    // Greater than 3, quit!
+}
 ```
 
--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,26 @@
 
-// Our example enum
-enum Foo {
-    Bar,
-    Baz,
-    Qux(u32)
-}
+
 
 fn main() {
-    // All have type Option<i32>
-    let number = Some(7);
-    let letter: Option<i32> = None;
-    let emoticon: Option<i32> = None;
-    // The if let construct reads: if let destructures number into
-    // Some(i), evaluate the block({}).
-    if let Some(i) = number {
-        println!("Matched {:?}", i);
+    // Make optional of type Option<i32>
+    let mut optional = Some(0);
+    // This reads: while let destrutures optional into
+    // Some(i), evaluate the block({}). Else break.
+    while let Some(i) = optional {
+        if i > 3 {
+            println!("Greater than 3, quit!");
+            optional = None;
+        } else {
+            println!("i is {:?}. Try again.", i);
+            optional = Some(i + 1);
+        }
+        // ^ Less rightward drift and doesn't require
+        // explicitly handling the failing case.
     }
-    //=> Matched 7
-    // If you need to specify a failure, use an else:
-    if let Some(i) = letter {
-        println!("Matched {:?}", i);
-    } else {
-        // Destructure failed. Change to the failure case.
-        println!("Didn't match a number. Let's go with a letter!");
-    }
-    //=> Didn't match a number. Let's go with a letter!
-
-    // Provide an altered failing condition.
-    let i_like_letters = false;
-    if let Some(i) = emoticon {
-        println!("Matched {:?}!", i);
-        // Destructure failed. Evaluate an else if condition to see if the
-        // alternate failure branch should be taken:
-    } else if i_like_letters {
-        println!("Didn't match a number. Let's go with a letter!");
-    } else {
-        // The condition evaluated false. This branch is the default:
-        println!("I don't like letters. Let's go with an emoticon :)!");
-    }
-    //=> I don't like letters. Let's go with an emoticon :)!
-
-    // Create example variables
-    let a = Foo::Bar;
-    let b = Foo::Baz;
-    let c = Foo::Qux(100);
-    // Variable a match Foo::Bar
-    if let Foo::Bar = a {
-        println!("a is foobar");
-    }
-    //=> a is foobar
-    // Variable b does not match Foo::Bar
-    if let Foo::Bar = b {
-        println!("b is foobar");
-    }
-    // Variable c matches Foo::Qux which has a value
-    // Similar to Some() in the previous example
-    if let Foo::Qux(value) = c {
-        println!("c is {}", value);
-    }
-    //=> c is 100
-    // Binding also works with if let
-    if let Foo::Qux(value @ 100) = c {
-        println!("c is one hundred");
-    }
-    //=> c is one hundred
+    // i is 0. Try again.
+    // i is 1. Try again.
+    // i is 2. Try again.
+    // i is 3. Try again.
+    // Greater than 3, quit!
 }
 
