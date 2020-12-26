d73867d8a1a5a76d845e6582b37c1cdb462353d3
congnghia0609 <congnghia0609@gmail.com>
2020-12-27 01:17:48 +0700

    add Functions Closures

--- README.md
+++ README.md
@@ -1239,3 +1239,33 @@ fn main() {
 }
 ```
 
+#### 5.2. Closures (lambda expressions or lambdas)
+```rust
+fn main() {
+    // Increment via closures and functions.
+    fn function(i: i32) -> i32 {i + 1}
+    // Closures are anonymous, here we are binding them to references
+    // Annotation is identical to function annotation but is optional
+    // as are the {} wrapping the body. These nameless functions
+    // are assigned to appropriately named variables.
+    let closure_annotated = |i: i32| -> i32 {i + 1};
+    let closure_inferred = |i| i + 1;
+
+    let i = 1;
+    // Call the function and closures.
+    println!("function: {}", function(i));
+    println!("closure_annotated: {}", closure_annotated(i));
+    println!("closure_inferred: {}", closure_inferred(i));
+    //=> function: 2
+    //=> closure_annotated: 2
+    //=> closure_inferred: 2
+
+    // A closure taking no arguments which returns an i32.
+    // The return type is inferred.
+    let one = || 1;
+    println!("closure returning one: {}", one());
+    //=> closure returning one: 1
+}
+```
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,98 +1,29 @@
 
-struct Point {
-    x: f64,
-    y: f64,
-}
 
-// Implementation block, all Point methods go in here
-impl Point {
-    // This is a static method
-    // Static methods don't need to be called by an instance
-    // These methods are generally used as constructors
-    fn origin() -> Point {
-        Point{x: 0.0, y: 0.0}
-    }
-
-    // Another static method, taking two arguments:
-    fn new(x: f64, y: f64) -> Point {
-        Point{x, y}
-    }
-}
-
-struct Rectangle {
-    p1: Point,
-    p2: Point,
-}
-
-impl Rectangle {
-    // This is an instance method
-    // &self is sugar for self: &Self, where Self is the type of the
-    // caller object. In this case Self = Rectangle
-    fn area(&self) -> f64 {
-        // self gives access to the struct fields via the dot operator
-        let Point{x: x1, y: y1} = self.p1;
-        let Point{x: x2, y: y2} = self.p2;
-        // abs is a f64 method that returns the absolute value of the caller
-        ((x1 - x2) * (y1 - y2)).abs()
-    }
-
-    fn perimeter(&self) -> f64 {
-        let Point{x: x1, y: y1} = self.p1;
-        let Point{x: x2, y: y2} = self.p2;
-        2.0 * ((x1 - x2).abs() + (y1 - y2).abs())
-    }
-
-    // This method requires the caller object to be mutable
-    // &mut self desugars to self:: &mut Self
-    fn translate(&mut self, x: f64, y: f64) {
-        self.p1.x += x;
-        self.p2.x += x;
-        self.p1.y += y;
-        self.p2.y += y;
-    }
-}
-
-// Pair owns resources: two heap allocated integers
-struct Pair(Box<i32>, Box<i32>);
-
-impl Pair {
-    // This method consumes the resources of the caller object
-    // self desugars to self: Self.
-    fn destroy(self) {
-        // Destructure self
-        let Pair(first, second) = self;
-        println!("Destroying Pair({}, {})", first, second);
-        // first and second go out of scope and get freed
-    }
-}
 
 fn main() {
-    let rectangle = Rectangle {
-        // Static methods are called using double colons
-        p1: Point::origin(),
-        p2: Point::new(3.0, 4.0),
-    };
-    // Instance methods are called using the dot operator
-    // Note that the first argument &self is implicitly passed, i.e.
-    // rectangle.perimeter() === Rectangle::perimeter(&rectangle)
-    println!("Rectangle perimeter: {}", rectangle.perimeter());
-    println!("Rectangle area: {}", rectangle.area());
+    // Increment via closures and functions.
+    fn function(i: i32) -> i32 {i + 1}
+    // Closures are anonymous, here we are binding them to references
+    // Annotation is identical to function annotation but is optional
+    // as are the {} wrapping the body. These nameless functions
+    // are assigned to appropriately named variables.
+    let closure_annotated = |i: i32| -> i32 {i + 1};
+    let closure_inferred = |i| i + 1;
 
-    let mut square = Rectangle {
-        p1: Point::origin(),
-        p2: Point::new(1.0, 1.0),
-    };
-    // Error! `rectangle` is immutable, but this method requires a mutable
-    // object
-    // rectangle.translate(1.0, 0.0);
-    // TODO ^ Try uncommenting this line
-    // Okay! Mutable objects can call mutable methods
-    square.translate(1.0, 1.0);
+    let i = 1;
+    // Call the function and closures.
+    println!("function: {}", function(i));
+    println!("closure_annotated: {}", closure_annotated(i));
+    println!("closure_inferred: {}", closure_inferred(i));
+    //=> function: 2
+    //=> closure_annotated: 2
+    //=> closure_inferred: 2
 
-    let pair = Pair(Box::new(1), Box::new(2));
-    pair.destroy();
-    // Error! Previous `destroy` call "consumed" `pair`
-    // pair.destroy();
-    // TODO ^ Try uncommenting this line
+    // A closure taking no arguments which returns an i32.
+    // The return type is inferred.
+    let one = || 1;
+    println!("closure returning one: {}", one());
+    //=> closure returning one: 1
 }
 
