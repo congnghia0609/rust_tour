469f6e459c3804d7e7caf77f1f7db71404f80731
congnghia0609 <congnghia0609@gmail.com>
2020-12-30 15:06:00 +0700

    add Pipes

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,35 @@
-use std::process::Command;
+use std::io::prelude::*;
+use std::process::{Command, Stdio};
+
+static PANGRAM: &'static str =
+    "the quick brown fox jumped over the lazy dog\n";
 
 fn main() {
-    let output = Command::new("rustc")
-        .arg("--version")
-        .output().unwrap_or_else(|e| {
-        panic!("failed to execute process: {}", e)
-    });
-    if output.status.success() {
-        let s = String::from_utf8_lossy(&output.stdout);
-        print!("rustc succeeded and stdout was:\n{}", s);
-    } else {
-        let s = String::from_utf8_lossy(&output.stderr);
-        print!("rustc failed and stderr was:\n{}", s);
+    // Spawn the `wc` command
+    let process = match Command::new("wc")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn() {
+        Err(why) => panic!("couldn't spawn wc: {}", why),
+        Ok(process) => process,
+    };
+    // Write a string to the `stdin` of `wc`.
+    //
+    // `stdin` has type `Option<ChildStdin>`, but since we know this instance
+    // must have one, we can directly `unwrap` it.
+    match process.stdin.unwrap().write_all(PANGRAM.as_bytes()) {
+        Err(why) => panic!("couldn't write to wc stdin: {}", why),
+        Ok(_) => println!("sent pangram to wc"),
     }
-    //=> rustc succeeded and stdout was:
-    //=> rustc 1.47.0 (18bf6b4f0 2020-10-07)
+    // Because `stdin` does not live after the above calls, it is `drop`ed,
+    // and the pipe is closed.
+    //
+    // This is very important, otherwise `wc` wouldn't start processing the
+    // input we just sent.
+    // The `stdout` field also has type `Option<ChildStdout>` so must be unwrapped.
+    let mut s = String::new();
+    match process.stdout.unwrap().read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read wc stdout: {}", why),
+        Ok(_) => print!("wc responded with:\n{}", s),
+    }
 }
